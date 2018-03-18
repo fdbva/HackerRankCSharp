9a2343d21b8ba9897c63880f1ec21a018b868d4e
Felipe Villela <fdbva@users.noreply.github.com>
2018-03-17 21:59:54 -0300

    Improved using string constructor and PadLeft

--- Practice/Algorithms/Warmup/Staircase.cs
+++ Practice/Algorithms/Warmup/Staircase.cs
@@ -4,24 +4,11 @@ using System.IO;
 using System.Linq;
 class Solution
 {
-
-    static void staircase(int n)
-    {
-        for (var i = 1; i < n+1; i++)
-        {
-            for (var j = n; j > 0; j--)
-            {
-                Console.Write(i < j ? " " : "#");
-            }
-
-            Console.WriteLine();
-        }
-    }
-
     static void Main(String[] args)
     {
         int n = Convert.ToInt32(Console.ReadLine());
-        staircase(n);
+        for (var i = 1; i < n + 1; i++)
+            Console.WriteLine(new string('#', i).PadLeft(n, ' '));
     }
 }
 
